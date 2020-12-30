4eab1f1a40360f67a861acbe69ed28233a6b4b97
Tomer Zaidler <tomerz@rightcodelabs.com>
2020-12-30 09:28:20 +0200

    added Access control to prevent CORS policy error

--- src/server.js
+++ src/server.js
@@ -4,6 +4,11 @@ const app = express();
 
 
 // app.use(express.urlencoded({ extended: false }));
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 app.use(express.json());
 
 const router = require('./routes/routes');
